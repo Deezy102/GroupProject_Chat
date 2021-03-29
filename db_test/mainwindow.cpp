01d887badc45c03eaeb37c4e67cd87ead43a90e8
TrttiTrttu <71984695+TrttiTrttu@users.noreply.github.com>
2021-03-29 17:28:50 +0300

    interface auth

---
+++ db_test/mainwindow.cpp
@@ -0,0 +1,54 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+#include <QMessageBox>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void check(string name, string password)
+{
+    QMessageBox msg;
+    map<string,string> buf = read_from_file();
+    map<string, string>::iterator iter;
+    auto search = buf.find(name);
+    if (search != buf.end() && (*search).second == password)
+    {
+        msg.setWindowTitle("chat");
+        msg.setText("entry...");
+        msg.exec();
+    }
+    else
+    {
+        msg.setWindowTitle("chat");
+        msg.setText("incorrect login or password");
+        msg.exec();
+    }
+}
+
+void MainWindow::on_signin_but_clicked()
+{
+    QString login = ui->log_line->text();
+    QString password = ui->pass_line->text();
+    QMessageBox msg;
+    check(login.toStdString(),password.toStdString());
+}
+
+void MainWindow::on_signup_but_clicked()
+{
+    hide();
+    reg = new reg_w(this);
+    reg->show();
+    this->show();
+}
---
+++ db_test/mainwindow.h
@@ -0,0 +1,37 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QDebug>
+#include <QErrorMessage>
+#include <QString>
+#include <QMessageBox>
+#include <reg_w.h>
+
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_signin_but_clicked();
+
+    void on_signup_but_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    reg_w *reg;
+
+};
+
+void check(map<string,string> mp);
+
+#endif // MAINWINDOW_H
